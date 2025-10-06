ea7147144430bbe6a769ea15e9fcb66d2002d1c2
Rina <st101470@student.spbu.ru>
2025-10-06 10:27:42 +0000

    3 python flies

---
+++ .ipynb_checkpoints/apple-checkpoint.py
@@ -0,0 +1,5 @@
+#price for apples
+price_per_apple = 15.5
+apples = int(input("How many apples do you want? "))
+total = price_per_apple * apples
+print("The total cost is", total)
---
+++ dessert.py
@@ -0,0 +1,10 @@
+#can I eat dessert
+has_dessert = True
+is_dieting = False
+
+if has_dessert and not is_dieting:
+    print("Time for dessert ")
+elif is_dieting:
+    print("Only one bite")
+else:
+    print("No dessert today")
